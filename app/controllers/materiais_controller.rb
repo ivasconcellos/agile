7b79b70603f6814ded374f820caafa1d3d443d64
isadora <ivasconcellos@iff.edu.br>
2018-07-08 18:58:12 -0300

    Configurado controller de materiais.

--- app/controllers/materiais_controller.rb
+++ app/controllers/materiais_controller.rb
@@ -1,24 +1,29 @@
 class MateriaisController < ApplicationController
   before_action :set_material, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_usuario!
 
   # GET /materiais
   # GET /materiais.json
   def index
-    @materiais = Material.all
+    @materiais = Material.all.page(params[:page]).order('nome')
+    authorize! :index, @materiais
   end
 
   # GET /materiais/1
   # GET /materiais/1.json
   def show
+    authorize! :show, @material
   end
 
   # GET /materiais/new
   def new
     @material = Material.new
+    authorize! :new, @material
   end
 
   # GET /materiais/1/edit
   def edit
+    authorize! :edit, @material
   end
 
   # POST /materiais
@@ -28,7 +33,7 @@ class MateriaisController < ApplicationController
 
     respond_to do |format|
       if @material.save
-        format.html { redirect_to @material, notice: 'Material was successfully created.' }
+        format.html { redirect_to @material, notice: 'Material criado com sucesso!' }
         format.json { render :show, status: :created, location: @material }
       else
         format.html { render :new }
@@ -42,7 +47,7 @@ class MateriaisController < ApplicationController
   def update
     respond_to do |format|
       if @material.update(material_params)
-        format.html { redirect_to @material, notice: 'Material was successfully updated.' }
+        format.html { redirect_to @material, notice: 'Material atualizado com sucesso!' }
         format.json { render :show, status: :ok, location: @material }
       else
         format.html { render :edit }
@@ -54,9 +59,10 @@ class MateriaisController < ApplicationController
   # DELETE /materiais/1
   # DELETE /materiais/1.json
   def destroy
+    authorize! :destroy, @material
     @material.destroy
     respond_to do |format|
-      format.html { redirect_to materiais_url, notice: 'Material was successfully destroyed.' }
+      format.html { redirect_to materiais_url, notice: 'Material excluído com sucesso!' }
       format.json { head :no_content }
     end
   end
