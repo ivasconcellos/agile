dbf97d95ad7a98f031e1567f06da9aaffcc8778e
isadora <ivasconcellos@iff.edu.br>
2018-07-08 18:58:22 -0300

    Configurada migração de materiais.

--- db/migrate/20180708214012_create_materiais.rb
+++ db/migrate/20180708214012_create_materiais.rb
@@ -1,9 +1,9 @@
 class CreateMateriais < ActiveRecord::Migration[5.2]
   def change
     create_table :materiais do |t|
-      t.string :nome
-      t.text :descricao
-      t.string :link
+      t.string :nome, null: false
+      t.text :descricao, null: false
+      t.string :link, null: false
       t.references :conteudo, foreign_key: true
 
       t.timestamps
