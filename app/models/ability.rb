ad157de61d5b80e38279cf898f1f252248c0453e
isadora <ivasconcellos@iff.edu.br>
2018-07-08 12:26:55 -0300

    Configuradas permissões iniciais.

---
+++ app/models/ability.rb
@@ -0,0 +1,13 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(usuario)
+    alias_action :create, :read, :update, to: :perms_without_delete
+    usuario ||= Usuario.new # guest user (not logged in)
+    if usuario.perfil == 'Professor'
+      can :manage, :all
+    elsif usuario.perfil == 'Aluno'
+        can :read, :all
+    end
+  end
+end
