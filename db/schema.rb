2f00aad266cccfdc97f287312837412af0084a17
isadora <ivasconcellos@iff.edu.br>
2018-07-08 18:36:23 -0300

    Configurado templates de curso

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_08_190213) do
+ActiveRecord::Schema.define(version: 2018_07_08_204442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,15 @@ ActiveRecord::Schema.define(version: 2018_07_08_190213) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "conteudos", force: :cascade do |t|
+    t.string "nome", null: false
+    t.text "descricao"
+    t.bigint "curso_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["curso_id"], name: "index_conteudos_on_curso_id"
   end
 
   create_table "cursos", force: :cascade do |t|
@@ -101,5 +110,6 @@ ActiveRecord::Schema.define(version: 2018_07_08_190213) do
     t.index ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "conteudos", "cursos"
   add_foreign_key "cursos", "tema_cursos"
 end
