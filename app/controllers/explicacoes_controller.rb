f2c3c83f5ee4926d4764c22fc3331fe9721ffacc
isadora <ivasconcellos@iff.edu.br>
2018-07-08 20:12:45 -0300

    Configurado controllers.

--- app/controllers/explicacoes_controller.rb
+++ app/controllers/explicacoes_controller.rb
@@ -1,24 +1,28 @@
 class ExplicacoesController < ApplicationController
   before_action :set_explicacao, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_usuario!
   # GET /explicacoes
   # GET /explicacoes.json
   def index
-    @explicacoes = Explicacao.all
+    @explicacoes = Explicacao.all.page(params[:page]).order('nome')
+    authorize! :index, @explicacoes
   end
 
   # GET /explicacoes/1
   # GET /explicacoes/1.json
   def show
+    authorize! :show, @explicacao
   end
 
   # GET /explicacoes/new
   def new
     @explicacao = Explicacao.new
+    authorize! :new, @explicacao
   end
 
   # GET /explicacoes/1/edit
   def edit
+    authorize! :edit, @explicacao
   end
 
   # POST /explicacoes
@@ -28,7 +32,7 @@ class ExplicacoesController < ApplicationController
 
     respond_to do |format|
       if @explicacao.save
-        format.html { redirect_to @explicacao, notice: 'Explicacao was successfully created.' }
+        format.html { redirect_to @explicacao, notice: 'Explicação criada com sucesso!' }
         format.json { render :show, status: :created, location: @explicacao }
       else
         format.html { render :new }
@@ -42,7 +46,7 @@ class ExplicacoesController < ApplicationController
   def update
     respond_to do |format|
       if @explicacao.update(explicacao_params)
-        format.html { redirect_to @explicacao, notice: 'Explicacao was successfully updated.' }
+        format.html { redirect_to @explicacao, notice: 'Explicação atualizada com sucesso!' }
         format.json { render :show, status: :ok, location: @explicacao }
       else
         format.html { render :edit }
@@ -54,9 +58,10 @@ class ExplicacoesController < ApplicationController
   # DELETE /explicacoes/1
   # DELETE /explicacoes/1.json
   def destroy
+    authorize! :destroy, @explicacao
     @explicacao.destroy
     respond_to do |format|
-      format.html { redirect_to explicacoes_url, notice: 'Explicacao was successfully destroyed.' }
+      format.html { redirect_to explicacoes_url, notice: 'Explicação excluída com sucesso!' }
       format.json { head :no_content }
     end
   end
