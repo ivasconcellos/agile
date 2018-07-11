2fce437b97eb8ca6d09e65dff57d78f434595ca3
isadora <ivasconcellos@iff.edu.br>
2018-07-10 22:24:25 -0300

    Adicionado campo codigo_acesso no curso.

--- app/models/curso.rb
+++ app/models/curso.rb
@@ -1,6 +1,7 @@
 class Curso < ApplicationRecord
-	validates_presence_of :nome, :tema_curso_id, :proprietario_id
 	belongs_to :tema_curso
 	belongs_to :proprietario, class_name: "Usuario"
+	validates_presence_of :nome, :tema_curso_id, :proprietario_id, :codigo_acesso
+	validates_length_of :codigo_acesso, :minimum => 8, :maximum => 8
   	self.per_page = 10
 end
