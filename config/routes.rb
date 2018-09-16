235d673beb46e77511ffff971b977468ce010817
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2018-09-16 20:30:11 -0300

    Configuradas novas rotas

--- config/routes.rb
+++ config/routes.rb
@@ -19,9 +19,11 @@ Rails.application.routes.draw do
 
   get 'inicial' => 'controle#inicial'
   get 'sobre' => 'controle#sobre'
+  get 'desenvolvimento' => 'controle#desenvolvimento'
   get 'curso_atual' => 'cursos#atualizar_curso_atual'
   get 'busca_curso' => 'usuario_curso#busca_curso'
   get 'descricao' => 'cursos#descricao'
+  get 'material_texto' => 'materiais#texto'
  
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
