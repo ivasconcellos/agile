5f01ab1174738c1bda42359caeb19153b5d54fe0
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2018-12-10 18:06:00 -0200

    Configurado models de fórum e mensagem.

--- app/models/comentario.rb
+++ app/models/comentario.rb
@@ -1,5 +1,5 @@
 class Comentario < ApplicationRecord
-  belongs_to :usuario
+  belongs_to :usuario_curso
   belongs_to :forum
-  validates_presence_of :texto, :forum_id, :usuario_id
+  validates_presence_of :texto, :forum_id, :usuario_curso_id
 end
