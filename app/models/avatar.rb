2aa610f0c023b8f4a3d4cde9a1e0a9a1b25f9435
ivasconcellos <ivasconcellos@iff.edu.br>
2018-12-10 22:18:52 -0200

    Configurados models

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -1,3 +1,6 @@
 class Avatar < ApplicationRecord
   belongs_to :tema_curso
+  validates_presence_of :nome, :tema_curso_id, :imagem
+  has_attached_file :imagem, styles: {medium: "300x300", thumb:"100x100"}, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :imagem, content_type: ["image/png", "image/jpg", "image/jpeg"], message: "Tipo de arquivo não permitido(Escolha um arquivo png ou jpg)"
 end
--- app/models/usuario_curso.rb
+++ app/models/usuario_curso.rb
@@ -2,7 +2,7 @@ class UsuarioCurso < ApplicationRecord
 	validates_presence_of :perfil, :nickname
 	belongs_to :curso
 	belongs_to :usuario
-	has_attached_file :avatar, styles: {medium: "300x300", thumb:"100x100"}, default_url: "/images/:style/missing.png"
+	has_attached_file :avatar, styles: {medium: "300x300>", thumb:"50x50>"}, default_url: "missing.png"
 	validates_attachment_content_type :avatar, content_type: ["image/png", "image/jpg", "image/jpeg"], message: "Tipo de arquivo não permitido(Escolha um arquivo png ou jpg)"
 	validates_uniqueness_of :usuario_id, scope: :curso_id, message: "já está cadastrado no curso!"
 end
