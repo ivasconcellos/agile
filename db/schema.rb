7a32403fbed49a2433ca913de40e68b520d849e5
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2018-12-27 17:59:37 -0200

    Configurado campo ativo no cadastro do usuário.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_011036) do
+ActiveRecord::Schema.define(version: 2018_12_27_171853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -208,6 +208,7 @@ ActiveRecord::Schema.define(version: 2018_12_11_011036) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "curso_atual_id"
+    t.boolean "ativo", default: true
     t.index ["curso_atual_id"], name: "index_usuarios_on_curso_atual_id"
     t.index ["email"], name: "index_usuarios_on_email", unique: true
     t.index ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true
