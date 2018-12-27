3402189d1db77e8fefa79714b1955c5ae6e4831e
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2018-12-27 17:58:01 -0200

    Configuradas permissões e model avatar

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,23 +1,27 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(usuario)
+  def initialize(usuario_curso)
     alias_action :create, :read, :update, to: :perms_without_delete
-    usuario ||= Usuario.new # guest user (not logged in)
-    if usuario.perfil == 'Gestor'
-      can :read, :all
-      can :perms_without_delete, Curso
-      #can :perms_without_delete, TemaCurso
+    alias_action :read, :update, to: :perms_read_and_update
+    usuario_curso ||= UsuarioCurso.new # guest user (not logged in)
+    if usuario_curso.perfil == 'Professor'
+      can :manage, Curso
       can :manage, Conteudo
       can :manage, Explicacao
       can :manage, Material
       can :manage, UsuarioCurso
       can :manage, Forum
       can :manage, Comentario
+      can :manage, TemaCurso
+      can :manage, Avatar
+    elsif usuario_curso.perfil == 'Aluno'
+      can :manage, Comentario
+      can :perms_read_and_update, UsuarioCurso
+    end
+
+    if usuario_curso.usuario.perfil == 'Gestor'
       can :manage, :all
-    elsif usuario.perfil == 'Usuário'
-        can :manage, Comentario
-        can :read, :all
     end
   end
 end
