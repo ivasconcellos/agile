44bbd0332425bd6bb2fd449f62572fdf1e00a148
ivasconcellos <ivasconcellos@iff.edu.br>
2018-12-27 22:17:26 -0200

    Configuradas migrações

--- db/migrate/20180708204442_create_modulos.rb
+++ db/migrate/20180708204442_create_modulos.rb
@@ -3,6 +3,7 @@ class CreateModulos < ActiveRecord::Migration[5.2]
     create_table :modulos do |t|
       t.string :nome
       t.text :descricao
+      t.boolean :publico, default: true
       t.references :curso, foreign_key: true
 
       t.timestamps
--- db/migrate/20180912031605_add_tipo_to_materiais.rb
+++
@@ -1,5 +0,0 @@
-class AddTipoToMateriais < ActiveRecord::Migration[5.2]
-  def change
-    add_column :materiais, :tipo, :string
-  end
-end
