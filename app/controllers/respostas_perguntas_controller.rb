8b87210d6140049c6ace4a94e351a8160cd5ef6f
ivasconcellos <ivasconcellos@iff.edu.br>
2019-02-07 11:30:22 -0200

    Cnfigurada exibição da resposta_pergunta.

--- app/controllers/respostas_perguntas_controller.rb
+++ app/controllers/respostas_perguntas_controller.rb
@@ -2,6 +2,12 @@ class RespostasPerguntasController < ApplicationController
   before_action :set_resposta_pergunta, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_usuario!
 
+  # GET /respostas_perguntas/1
+  # GET /respostas_perguntas/1.json
+  def show
+    authorize! :show, RespostaPergunta
+  end
+
   # GET /respostas_perguntas/new
   def new
     @resposta_pergunta = RespostaPergunta.new
