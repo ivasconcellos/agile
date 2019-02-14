885a9f0c2d1d6839e1d9b2261b308783afdb7d0d
ivasconcellos <ivasconcellos@iff.edu.br>
2019-02-14 11:18:03 -0200

    Removidos arquivos desnecessários

--- db/schema.rb
+++ db/schema.rb
@@ -54,16 +54,6 @@ ActiveRecord::Schema.define(version: 2019_02_14_125343) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
-  create_table "aluno_resposta", force: :cascade do |t|
-    t.bigint "usuario_curso_id"
-    t.bigint "respostas_perguntas_id"
-    t.boolean "correta", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["respostas_perguntas_id"], name: "index_aluno_resposta_on_respostas_perguntas_id"
-    t.index ["usuario_curso_id"], name: "index_aluno_resposta_on_usuario_curso_id"
-  end
-
   create_table "aluno_respostas", force: :cascade do |t|
     t.bigint "usuario_curso_id"
     t.bigint "respostas_perguntas_id"
@@ -360,8 +350,6 @@ ActiveRecord::Schema.define(version: 2019_02_14_125343) do
     t.index ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "aluno_resposta", "respostas_perguntas", column: "respostas_perguntas_id"
-  add_foreign_key "aluno_resposta", "usuario_curso"
   add_foreign_key "aluno_respostas", "respostas_perguntas", column: "respostas_perguntas_id"
   add_foreign_key "aluno_respostas", "usuario_curso"
   add_foreign_key "avaliacao_tarefa", "tarefa_alunos"
