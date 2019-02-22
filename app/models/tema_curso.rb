25ea1e50d5d841cb4b6bbbaaea1ec55f3e3e8659
ivasconcellos <ivasconcellos@iff.edu.br>
2019-02-21 22:59:11 -0300

    Configurado tamanho máximo das imagens.

--- app/models/tema_curso.rb
+++ app/models/tema_curso.rb
@@ -2,7 +2,7 @@ class TemaCurso < ApplicationRecord
 	validates_presence_of :nome, :cor_titulo, :cor_texto, :cor_fundo, :imagem
 
 	has_one_attached :imagem
-	validates :imagem, attached: true
+	validates :imagem, attached: true, size: { less_than: 10.megabytes , message: 'Imagem muito grande. Máximo de 10 MB.' }
 			
 	self.per_page = 10
 end
