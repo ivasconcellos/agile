0b0df55a5b1553e53240f04fc277b70fc81ede26
ivasconcellos <ivasconcellos@iff.edu.br>
2019-02-22 11:12:10 -0300

    Configurado helpers com data e hora.

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -4,5 +4,13 @@ class Quiz < ApplicationRecord
 
   self.per_page = 10
 
-  validates_presence_of :nome, :descricao, :max_tentativas
+  validates_presence_of :nome, :descricao, :max_tentativas, :data_inicio, :hora_inicio, :data_termino, :hora_termino
+
+  	def data_hora_inicio
+		self.data_inicio.strftime("%d/%m/%Y") + " " + self.hora_inicio.strftime("%H:%M:%S")
+	end
+
+	def data_hora_termino
+		self.data_termino.strftime("%d/%m/%Y") + " " + self.hora_termino.strftime("%H:%M:%S")
+	end
 end
