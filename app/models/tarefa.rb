1624f538dfd51d475cd51dbec2fc40fb272bf38f
ivasconcellos <ivasconcellos@iff.edu.br>
2019-03-06 23:41:38 -0300

    Configurada tarefa.

--- app/models/tarefa.rb
+++ app/models/tarefa.rb
@@ -9,11 +9,11 @@ class Tarefa < ApplicationRecord
   validates_presence_of :nome, :descricao, :data_inicio, :hora_inicio, :data_termino, :hora_termino
 
 	def data_hora_inicio
-		self.data_inicio.strftime("%d/%m/%Y") + " " + self.hora_inicio.strftime("%H:%M:%S")
+		self.data_inicio.strftime("%d/%m/%Y") + " - " + self.hora_inicio.strftime("%H:%M:%S")
 	end
 
 	def data_hora_termino
-		self.data_termino.strftime("%d/%m/%Y") + " " + self.hora_termino.strftime("%H:%M:%S")
+		self.data_termino.strftime("%d/%m/%Y") + " - " + self.hora_termino.strftime("%H:%M:%S")
 	end
 
 end
