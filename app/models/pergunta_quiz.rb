8aedb1ed6b5e70e304cd105e503fbd89f344409f
ivasconcellos <ivasconcellos@iff.edu.br>
2019-03-09 19:33:20 -0300

    Configurada validação da pontuação da pergunta.

--- app/models/pergunta_quiz.rb
+++ app/models/pergunta_quiz.rb
@@ -2,4 +2,15 @@ class PerguntaQuiz < ApplicationRecord
   belongs_to :quiz
   has_many :respostas_perguntas
   validates_presence_of :descricao, :pontuacao
+
+  validate :verificar_pontuacao_maxima
+
+  private
+
+  def verificar_pontuacao_maxima
+  		soma = PerguntaQuiz.where(quiz_id: self.quiz.id).sum(pontuacao)
+		if (self.pontuacao + soma) > self.quiz.missao.pontuacao
+			errors.add(:pontuacao, " - Pontuação da Pergunta ultrapassa a pontuação máxima estabelecida para a Missão!")
+		end
+	end
 end
