6cc78b18c37a70f245ee661e3fe9cc1d101267ed
ivasconcellos <ivasconcellos@iff.edu.br>
2019-03-13 11:13:39 -0300

    Configurada exibição de resultado da resposta do Quiz.

--- app/helpers/aluno_respostas_helper.rb
+++ app/helpers/aluno_respostas_helper.rb
@@ -2,9 +2,9 @@ module AlunoRespostasHelper
 
 	def resposta_correta(resposta)
 		if resposta.correta?
-			('<i class="fa fa-check-circle alert alert-success"> ' +  resposta.descricao + '</i>').html_safe
+			('<i class="fa fa-check-circle text-success"> ' +  resposta.descricao + ' - Correta!' + '</i>' ).html_safe
 		else
-			('<i class="fa fa-exclamation-circle alert alert-danger"> ' +  resposta.descricao + '</i>').html_safe
+			('<i class="fa fa-exclamation-circle text-danger"> ' +  resposta.descricao + ' - Incorreta!' +'</i>').html_safe
     	end
 	end
 
