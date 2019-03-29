de45993871409dd65c6f7e134a3910dec13b539b
ivasconcellos <ivasconcellos@iff.edu.br>
2019-03-28 23:23:48 -0300

    Configurado javascript.

--- app/models/modulo_ordenacao.rb
+++ app/models/modulo_ordenacao.rb
@@ -9,11 +9,11 @@ class ModuloOrdenacao < ApplicationRecord
 	def self.remover(objeto)
 		ModuloOrdenacao.find_by(modulo_id: objeto.modulo_id, classe: objeto.class.name, classe_id: objeto.id ).destroy
 	end
+	
 	def objeto
 		classe = self.classe.constantize
 		obj = classe.find_by(id: self.classe_id)
-		
 		return obj
 	end
-
+	
 end
