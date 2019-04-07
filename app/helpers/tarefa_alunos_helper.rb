9e197650823f4c000a01e219756df18f4ec1b9ef
ivasconcellos <ivasconcellos@iff.edu.br>
2019-04-06 22:02:23 -0300

    Configurada exibição de notas do aluno[

--- app/helpers/tarefa_alunos_helper.rb
+++ app/helpers/tarefa_alunos_helper.rb
@@ -2,11 +2,11 @@ module TarefaAlunosHelper
 
 	def avaliada(tarefa, usuario)
         @tarefa = TarefaAluno.find_by("tarefa_id = ? and usuario_curso_id = ?", tarefa, usuario)
-		if @tarefa
-            return "Sim"
-        else
-            return "Não"
-        end
+		if @tarefa.avaliada
+			return "Sim"
+		else
+			return "Não"
+		end
     end
     
 	def enviada(tarefa, usuario)
