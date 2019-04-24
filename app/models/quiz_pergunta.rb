adbbf00f0de67667a12a521e1ab2776b577fd83c
ivasconcellos <ivasconcellos@iff.edu.br>
2019-04-23 22:51:05 -0300

    Configurado o envio de e-mail ao cadastrar um usuário no curso.

--- app/models/quiz_pergunta.rb
+++ app/models/quiz_pergunta.rb
@@ -3,18 +3,23 @@ class QuizPergunta < ApplicationRecord
   has_many :quiz_pergunta_respostas
   validates_presence_of :descricao, :pontuacao
 
+  validates :pontuacao, numericality: { only_integer: true }
+
   validate :verificar_pontuacao_maxima
 
   private
 
   def verificar_pontuacao_maxima
-    if self.id
-  		soma = QuizPergunta.where('quiz_id =? and id != ?',self.quiz.id, self.id).sum('pontuacao')
-    else
-      soma = QuizPergunta.where('quiz_id =?',self.quiz.id).sum('pontuacao')
-    end
+    
+    if self.pontuacao
+      if self.id
+        soma = QuizPergunta.where('quiz_id =? and id != ?',self.quiz.id, self.id).sum('pontuacao')
+      else
+        soma = QuizPergunta.where('quiz_id =?',self.quiz.id).sum('pontuacao')
+      end
   		if (self.pontuacao + soma) > self.quiz.missao.pontuacao
   			errors.add(:pontuacao, " - Pontuação da Pergunta ultrapassa a pontuação máxima estabelecida para a Missão!")
   		end
+    end
 	end
 end
