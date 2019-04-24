45f8c396e17ae624bc25ecf2defe2e8471da1a1d
ivasconcellos <ivasconcellos@iff.edu.br>
2019-04-24 01:16:31 -0300

    Realizadas correções.

--- app/models/forum_comentario.rb
+++ app/models/forum_comentario.rb
@@ -1,7 +1,7 @@
 class ForumComentario < ApplicationRecord
   belongs_to :usuario_curso
   belongs_to :forum
-  belongs_to :forum_comentario, :class_name => "ForumComentario"
+  belongs_to :forum_comentario, :class_name => "ForumComentario", optional: true
   has_many :foruns_comentarios, :foreign_key => "forum_comentario_id"
   
   validates_presence_of :texto, :forum_id, :usuario_curso_id
--- db/migrate/20181226143260_create_tarefa_alunos.rb
+++ db/migrate/20181226143260_create_tarefa_alunos.rb
@@ -3,7 +3,7 @@ class CreateTarefaAlunos < ActiveRecord::Migration[5.2]
     create_table :tarefa_alunos do |t|
       t.references :tarefa, foreign_key: true
       t.references :usuario_curso, foreign_key: true
-      t.boolean :avaliada, :boolean, default: false
+      t.boolean :avaliada, default: false
 
       t.timestamps
     end
