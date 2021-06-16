863c8bc9995e1220834fcfb031574363930ac803
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2021-06-16 14:16:10 -0300

    Configurado cálculo de pontuação geral e média por equipe

--- app/models/grupo_curso.rb
+++ app/models/grupo_curso.rb
@@ -7,11 +7,18 @@ class GrupoCurso < ApplicationRecord
   
   self.per_page = 10
 
-  def pontuacao_total
-  	UsuarioCurso.where(grupo_curso_id: self.id, curso_id: self.curso.id).sum('pontos_experiencia')
+  def pontuacao_total_equipe
+    pontuacao_total = 0
+  	alunos = UsuarioCurso.where(grupo_curso_id: self.id, curso_id: self.curso.id)
+    for aluno in alunos
+      pontuacao_total = pontuacao_total + aluno.pontuacao
+    end
+    return pontuacao_total
   end
 
-  def pontuacao_media
-  	UsuarioCurso.where(grupo_curso_id: self.id, curso_id: self.curso.id).average('pontos_experiencia')
+  def pontuacao_media_equipe
+    qnt_alunos_grupo = UsuarioCurso.where(grupo_curso_id: self.id, curso_id: self.curso.id).count
+  	media = self.pontuacao_total_equipe / qnt_alunos_grupo
+    return "%g" % ("%.2f" % media)
   end
 end
